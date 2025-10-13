d2b263fb3fba839484df89fa6d818f19ddfbeac4
elsaaulia1809-dev <elsaaulia1809@gmail.com>
2025-10-13 13:19:39 +0700

    latihan c++

---
+++ latihan.c++
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nama ="bayu";
+    int umur =19;
+    float tinggi=170.5;
+    bool aktif=true;
+
+    cout <<"nama:"<<nama<<endl;
+    cout <<"umur:"<<umur<<endl;
+    cout <<"tinggi:"<<tinggi<<endl;
+    cout <<"status aktif:"<<aktif<<endl;
+
+    return 0;
+
+}
---
+++ latihan.java
@@ -0,0 +1,14 @@
+public class main{
+    public static void main(string[] args){
+        string nama ="bayu";
+        int umur = 19;
+        double tinggi = 170.5;
+        boolean aktif = true;
+
+        System.out.println("nama:"+nama)
+        System.out.println("umur:"+umur)
+        System.out.println("tinggi:"+tinggi);
+        System.out.println("status aktif:"+aktif)
+
+    }
+}
