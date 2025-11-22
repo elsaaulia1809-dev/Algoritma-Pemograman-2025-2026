52e30fd2d5823933489aa5c1378027859d57f725
elsaaulia1809-dev <elsaaulia1809@gmail.com>
2025-11-22 18:38:12 +0700

    c++.c++

---
+++ c++.c++/tipedata1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nama = "Elsa aulia";
+    int umur = 17;
+    float tinggi = 160.5;
+    bool aktif = true;
+
+    cout << "Nama:" << nama << endl;
+    cout << "Umur:" << umur << endl;
+    cout << "Tinggi:" << tinggi << endl;
+    cout << "Status aktif:" << aktif << endl;
+    return 0;
+}
