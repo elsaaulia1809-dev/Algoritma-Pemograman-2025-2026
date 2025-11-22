d45691e3ad54022d2af74693bcdec6c606244afa
elsaaulia1809-dev <elsaaulia1809@gmail.com>
2025-11-22 18:40:05 +0700

    java

---
+++ java/tipedata.java
@@ -0,0 +1,13 @@
+public class tipedata {
+    public static void main(String[] args) {
+        String nama = "Elsa aulia";
+        int umur = 17;
+        double tinggi = 160.5;
+        boolean aktif = true;
+
+        System.out.println("Nama:" + nama);
+        System.out.println("Umur:" + umur);
+        System.out.println("Tinggi:" + tinggi);
+        System.out.println("Status aktif:" + aktif);
+    }
+}
