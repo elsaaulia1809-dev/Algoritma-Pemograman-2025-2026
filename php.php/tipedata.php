e9b336045f26a38316e1bdcd150c5650bfc80a73
elsaaulia1809-dev <elsaaulia1809@gmail.com>
2025-11-22 18:45:59 +0700

    php.php

---
+++ php.php/tipedata.php
@@ -0,0 +1,11 @@
+<?php
+$nama = "elsa aulia";
+$umur = 17;
+$tinggi = 160.5;
+$aktif = true;
+
+echo "nama: $nama<br>";
+echo "umur: $umur<br>";
+echo "tinggi: $tinggi<br>";
+echo "status aktif : $aktif";
+?>  
