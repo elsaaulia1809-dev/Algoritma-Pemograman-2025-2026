6a88b82b9ebb1530da715ed028ac299efe003b48
elsaaulia1809-dev <elsaaulia1809@gmail.com>
2025-12-01 10:58:12 +0700

    switch-case

---
+++ java/programATM.java
@@ -0,0 +1,10 @@
+public class programATM {
+    
+    public static void main(String[] args) {
+        System.out.println("SELAMAT DATANG DI ATM");
+        System.out.println("PILIH OPTION");
+        System.out.println("1. cek saldo saya ");  
+        System.out.println("2. ambil uang saya");
+    }                                                                               
+
+}
